70ccab13503d39e93dabafd96a620c9708ddbf61
Rosso-OmniForge <technical@blackboxwhite.co.za>
2025-11-06 20:28:55 +0200

    auto server start now

--- app.py
+++ app.py
@@ -270,4 +270,4 @@ if __name__ == '__main__':
     thread = threading.Thread(target=background_thread, daemon=True)
     thread.start()
     
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
